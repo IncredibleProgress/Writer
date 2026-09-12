47db013763d6c5e234cb7089d640d49635d518c9
IncredibleProgress <champion.nicolas@gmail.com>
2026-09-12 10:53:45 +0200

    edit to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
